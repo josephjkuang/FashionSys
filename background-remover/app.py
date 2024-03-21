f2e5f4464f90f88828ef6a9c9b478229d57b958f
josephjkuang <josephjkuang@gmail.com>
2024-03-21 00:36:19 -0500

    Add clothing classifier

--- background-remover/app.py
+++ background-remover/app.py
@@ -1,6 +1,9 @@
-from rembg import remove
 from PIL import Image
+from rembg import remove
 
+import os
+
+# removes background of image
 def remove_background(image_path):
     try:
         # Load the image using PIL
@@ -14,15 +17,29 @@ def remove_background(image_path):
     except Exception as e:
         print(f"Error removing background: {e}")
         return None  # Return None in case of errors
+    
+input_path = "../test_images"
+output_path = "../no_background_images"
 
-# Usage
+# Loads in files and does whole process of removing background and saving image.
+def remove_and_save(filename):
+    if filename.endswith(".png") or filename.endswith(".jpg"):
+        print("Processing", filename)
+        # Construct the full path to the image file
+        image_path = os.path.join(input_path, filename)
 
-image_path = "test_shorts.png"  # Replace with your image path
-img_without_bg = remove_background(image_path)
+        # Process the image
+        img_without_bg = remove_background(image_path)
+        
+        # Check if the background removal was successful
+        if img_without_bg is not None:
+            # Save the image with the transparent background
+            save_path = os.path.join(output_path, f"{filename}")
+            img_without_bg.save(save_path, format="PNG")
+        else:
+            print(f"Error removing background for image: {filename}")
 
-if img_without_bg is not None:
-    # Save the image with the transparent background
-    img_without_bg.save("image_without_background.png", format="PNG")
-    img_without_bg.show()
-else:
-    print("Error removing background. Check the image path and try again.")
+# Iterate over all files in the folder
+for filename in os.listdir(input_path):
+    remove_and_save(filename)
+    
