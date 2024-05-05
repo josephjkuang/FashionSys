ea11d61e119c7b12a527768754923c8fa8934161
xinshuo3 <xinshuo3@sp24-cs525-1701.cs.illinois.edu>
2024-05-05 16:25:57 -0500

    update measuerment

--- measurements/test_on_sever.py
+++ measurements/test_on_sever.py
@@ -4,7 +4,7 @@ import time
 start_time = time.time() 
 
 # URL of the API endpoint
-url = "http://3.15.239.14:8000/with_personalization/"
+url = "http://3.15.239.14:8000/full_prediction/"
 
 # Path to the file to be uploaded
 file_path = "/home/xinshuo3/FashionSys/measurements/samples/shoes.jpg"
